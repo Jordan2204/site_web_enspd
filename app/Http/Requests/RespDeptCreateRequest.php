15a25fc5d0c769d321e78d57e9c7f3858cdee949
Jordan2204 <anafackjordan@gmail.com>
2020-05-09 15:21:53 +0100

    Enfin Prete pour l'optimisation de la page d'accueil

---
+++ app/Http/Requests/RespDeptCreateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RespDeptCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed|min:6',
+            'refDept' => 'required|unique:respdepts'
+           
+        ];
+    }
+}
